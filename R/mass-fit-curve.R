0758d74ab1859eebc60c5af6c2c904db0b8a3b76
rmylonas <roman.mylonas@isb-sib.ch>
2018-06-27 10:18:40 +0200

    adapted doc

--- R/mass-fit-curve.R
+++ R/mass-fit-curve.R
@@ -26,11 +26,12 @@
 #' Filter the data and apply a fit on it.
 #'
 #' @param pg ProteinGroups data.frame.
-#' @param repeated_entries_threshold Remove entries which are repeated in more slices then the threshold (%).
-#' @param weak_intensity_threshold Remove entries which have an intensity below the threshold (%).
+#' @param repeated_entries_threshold Remove entries which are repeated in more
+#'   slices then the threshold.
+#' @param weak_intensity_threshold Remove entries which have an intensity below
+#'   the threshold.
 #' @examples
-#' proteinGroups_path <-
-#' system.file("extdata", "Conde_9508_sub_2.txt", package = "pumbaR")
+#' proteinGroups_path <- system.file("extdata", "Conde_9508_sub_2.txt", package = "pumbaR")
 #' pg <- load_MQ(proteinGroups_path)
 #' mass_fit <- filter_and_fit(pg)
 #' plot_fit(pg, mass_fit)
@@ -63,7 +64,8 @@ fit_curve <- function(pg){
 
 #' Filter_repeated_entries
 #'
-#' Remove proteins which were found in too many slices. Those are very usually contaminants.
+#' Remove proteins which were found in too many slices. Those are very usually
+#' contaminants.
 #'
 #' @param rep_threshold Max percentage of appearance. Default is 0.3.
 filter_repeated_entries <- function(pg, rep_threshold = 0.3){
@@ -80,7 +82,8 @@ filter_repeated_entries <- function(pg, rep_threshold = 0.3){
 #' Remove entries in regions with low density.
 #'
 #' @param pg MaxQuant ProteinGroup data.
-#' @param min_nr_threshold Min number of entries which should be found in a slice. Default is 10.
+#' @param min_nr_threshold Min number of entries which should be found in a
+#'   slice. Default is 10.
 #' @param step_nr Number of steps for the theoretical mass. Default is 500.
 filter_low_densities <- function(pg, min_nr_threshold = 10, step_nr = 500){
 
--- README.md
+++ README.md
@@ -1,22 +1,13 @@
 # pumbaR
 MaxQuant provides us with a file *proteinGroups.txt* which contains information about the theoretical masses of proteins and how abbundant peptides from those proteins are found in slices from one dimensional gels. This information can be used to compute a approximation of which masses correspond to each slice.
 
-## Prerequisites
-
-Install *PAFcontaminants* from GitHub using *devtools*:
-
-```R
-library(devtools)
-install_github("UNIL-PAF/pumbaR", build_vignettes = TRUE)
-```
-
 ## Installation
 
 Install *pumbaR* from source in R using *devtools*:
 
 ```R
 library(devtools)
-install_github("UNIL-PAF/PAFcontaminants", build_vignettes = TRUE)
+install_github("UNIL-PAF/pumbaR", build_vignettes = TRUE)
 ```
 
 ## Usage
